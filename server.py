8562214d8847eac70130c0a5a9e28cb638bf0b95
Zack2patate18 <rosemickey150@gmail.com>
2025-09-09 17:28:14 +0200

    added config file and fixed login route

--- server.py
+++ server.py
@@ -1,8 +1,21 @@
 import flask
 from flask import request, jsonify, render_template
+import json
+import os
 
 app = flask.Flask(__name__)
 
+if not os.path.exists('config.json'):
+    print("No config file found")
+    with open('config.json', 'w') as f:
+        f.write("{}")
+    exit(1)
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
 @app.route('/')
 def default():
-    return render_template('/templates/login.html')
+    return render_template('login.html')
+
+app.run(debug=True, port=config["port"], host="0.0.0.0")
